985e7c19db4d4a1db74aa68ee54d3f355a890301
Iain Barnett <iainspeed@gmail.com>
2012-03-07 02:05:13 +0000

    Routes for the tests

--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
 # encoding: UTF-8
 
 require 'sinatra'
-
+require 'sinatra/partial'
+    
 get "/" do
-  output = "HOME"
+  haml :form
 end
 
 post "/" do
-  output = params.inspect
+  haml :form, locals: {output: params.inspect}
+end
+
+get "/ajax" do
+  haml :ajaxform
+end
+
+post "/ajax" do
+  haml :ajaxform
+end
+
+before do
+  puts "path: #{request.path_info}"
+  puts "request.xhr? #{request.xhr?}"
+  puts "params: #{params.inspect}"
 end
